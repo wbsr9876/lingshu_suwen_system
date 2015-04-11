094ffbe6cb80198a4687a7a5025694f3078fd1a8
wbsr9876 <57223744wz@sina.com>
2015-04-11 12:25:03 +0800

    xls

---
+++ LSBuilder/LSDataCore/CommonDefine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSDataCore
+{
+    public class CommonDefine
+    {
+        //VERSION
+        public const uint VERSION = 0x1;
+        //CLASS_TYPE
+        public const int CT_DATAFILE = 0x10000;
+        public const int CT_SERVER = 0x20000;
+        public const int CT_CMAKEFILE = 0x30000;
+        //ERROR_CODE
+        public const int EC_SUCCESS = 0x0;
+        public const int EC_INVALID = 0x1;
+        public const int EC_MISMATCH = 0x2;
+        public const int EC_NOT_EXIST = 0x3;
+        public const int EC_UNKNOWN_ERROR = 0xFF;
+
+        //xml_string
+        public const string xs_root = "Workspace";
+        public const string xs_serverList = "ServerList";
+        public const string xs_server = "Server";
+        public const string xs_nameAttr = "Name";
+        public const string xs_countAttr = "Count";
+        public const string xs_pathAttr = "Path";
+    }
+}
---
+++ LSBuilder/LSDataCore/DataFile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LSDataCore
+{
+    public class DataFile
+    {
+        protected string path;
+        protected bool loaded = false;
+        protected uint version;
+        public int Load(string filePath)
+        {
+            path = filePath;
+            if (!File.Exists(path + "/LSData.dat"))
+                return CommonDefine.EC_NOT_EXIST | CommonDefine.CT_DATAFILE;
+            FileStream file = new FileStream(path + "/LSData.dat",FileMode.Open,FileAccess.Read);
+            using (BinaryReader binary = new BinaryReader(file))
+            {
+                try
+                {
+                    uint version = binary.ReadUInt32();
+                    if (version != CommonDefine.VERSION)
+                    {
+                        binary.Close();
+                        return CommonDefine.EC_MISMATCH | CommonDefine.CT_DATAFILE;
+                    }
+
+                    while(binary.BaseStream.Position < binary.BaseStream.Length)
+                    {
+                        int tableSize = binary.ReadInt32();
+                        if (tableSize > 0)
+                        {
+                            int dataSize = binary.ReadInt32();
+                            byte[] buffer = binary.ReadBytes(dataSize);
+                        }
+                    }
+                }
+                catch(System.Exception e)
+                {
+                    binary.Close();
+                    return CommonDefine.EC_UNKNOWN_ERROR | CommonDefine.CT_DATAFILE;
+                }
+            }
+            return 0;
+        }
+    }
+}
